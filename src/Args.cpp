8254adcf2e2d56d5eeeddef2d843268185991537
Clément <clement2104.boillot@gmail.com>
2024-01-27 10:19:43 +0100

    feat: refactor args parsing

--- src/Args.cpp
+++ src/Args.cpp
@@ -32,28 +32,23 @@ static void set_large_flag(options_t &options, const std::string &str)
     std::cerr << "Invalid option `--" << str << "`.\n" << std::flush;
 }
 
-static void get_option(options_t &options, char const *str)
-{
-    if (*str != '-') {
-        for (; *str != '\0'; str += 1)
-            set_short_flag(options, *str);
-        return;
-    }
-    str += 1;
-    set_large_flag(options, str);
-}
-
 options_t get_params(uint32_t argc, const char *argv[])
 {
     options_t options = 0;
     std::span args{argv, static_cast<std::size_t>(argc)};
 
     for (const auto &arg : args.subspan(1)) {
-        if (arg[0] == '-') {
-            get_option(options, arg + 1);
+        std::string str{arg};
+        if (str[0] != '-') {
+            std::cerr << "Invalid option `" << str << "`.\n" << std::flush;
             continue;
         }
-        std::cerr << "Invalid option `" << arg << "`.\n" << std::flush;
+        if (str[1] == '-') {
+            set_large_flag(options, str.substr(2));
+            continue;
+        }
+        for (size_t i = 1; str[i] != '\0'; i++)
+            set_short_flag(options, str[i]);
     }
     return options;
 }
